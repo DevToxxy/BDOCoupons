bae504e119f1a33ca81e2ed57213140b55cbc959
DevToxxy <63885882+DevToxxy@users.noreply.github.com>
2022-02-14 16:48:01 +0100

    Scraping expiration dates and codes working

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,6 +12,9 @@ using System.Net.Http.Headers;
 using System.Net;
 using System.Text;
 using System.IO;
+
+using AngleSharp;
+using AngleSharp.Html.Parser;
 namespace BDOCoupons.Controllers
 {
     public class HomeController : Controller
@@ -27,6 +30,7 @@ namespace BDOCoupons.Controllers
         {
             string url = "https://incendar.com/bdo_pearl_abyss_coupon_codes.php";
             var response = CallUrl(url).Result;
+            var linkList = ParseHtml(response);
             return View();
         }
 
@@ -47,8 +51,60 @@ namespace BDOCoupons.Controllers
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls13;
             client.DefaultRequestHeaders.Accept.Clear();
             var response = client.GetStringAsync(fullUrl);
+           
             return await response;
         }
-        
+        private List<string> ParseHtml(string html)
+        {
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+            
+            var rows = htmlDoc.DocumentNode.Descendants("input").Where(node => node.GetAttributeValue("id", "")
+                .Contains("incendar")).ToList();
+
+            List<string> couponCodeList = new List<string>(); //coupon codes sorted from newest
+
+            foreach (var row in rows)
+            {
+                couponCodeList.Add(row.GetAttributeValue("value", ""));
+            }
+
+            var couponsDesc = htmlDoc.DocumentNode.Descendants("table")
+                .Where((node) => !node.HasClass("headTable")).ToList();
+
+            var tr = couponsDesc[0].Descendants("tr").ToList();
+
+
+            var td = tr[0].Descendants("td").ToList();
+
+
+            List<string> dateList = new List<string>();
+            foreach (var tableDate in td)
+            {
+                dateList.Add(tableDate.InnerText);
+            }
+
+            int i = 0;
+            List<string> trueTableDateList = new List<string>(); //coupon expiration dates sorted from newest
+
+            foreach (var tabledata in dateList)
+            {
+                if(i % 3 == 0)
+                {
+                    if(i == 0)
+                    {
+                        trueTableDateList.Add(tabledata.Substring(0,5)); //hardcoded: change if owner of incendar also changes format
+                    }
+                    else
+                    {
+                        trueTableDateList.Add(tabledata);
+                    }
+                }
+                i++;
+            }
+            return null;
+
+        }
+
     }
 }
